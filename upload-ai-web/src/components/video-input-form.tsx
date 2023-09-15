5ba39389fcb1428a0173e5636e82af3401b1a1c1
Gabriela Carniel <gabiicarniel@gmail.com>
2023-09-15 14:05:07 -0300

    final commit

--- upload-ai-web/src/components/video-input-form.tsx
+++ upload-ai-web/src/components/video-input-form.tsx
@@ -8,8 +8,22 @@ import { getFFmpeg } from "@/lib/ffmpeg";
 import { fetchFile } from '@ffmpeg/util'
 import { api } from "@/lib/axios";
 
-export function VideoInputForm() {
+type Status = 'waiting' | 'converting' | 'uploading' | 'generating' | 'success'
+
+const statusMessages = {
+  'converting': 'Convertendo...',
+  'uploading': 'Carregando...',
+  'generating': 'Transcrevendo...',
+  'success': 'Sucesso...'
+}
+
+interface VideoInputFormProps {
+  onVideoUploaded: (id: string) => void
+}
+
+export function VideoInputForm({ onVideoUploaded }: VideoInputFormProps) {
   const [videoFile, setVideoFile] = useState<File | null>(null)
+  const [status, setStatus] = useState<Status>('waiting')
   const promptInputRef = useRef<HTMLTextAreaElement>(null)
 
 
@@ -71,6 +85,8 @@ export function VideoInputForm() {
 
     if (!videoFile) return
 
+    setStatus('converting')
+
     // converter o video em audio
 
     const audioFile = await convertVideoToAudio(videoFile)
@@ -78,9 +94,21 @@ export function VideoInputForm() {
     const data = new FormData()
     data.append('file', audioFile)
 
+    setStatus('uploading')
+
     const response = await api.post('/videos', data)
 
-    console.log(response.data)
+    const videoId = response.data.video.id
+
+    setStatus('generating')
+
+    await api.post(`/videos/${videoId}/transcription`, {
+      prompt,
+    })
+
+    setStatus('success')
+    onVideoUploaded(videoId)
+    console.log('finalizou')
   }
 
   const previewURL = useMemo(() => {
@@ -110,12 +138,20 @@ export function VideoInputForm() {
 
       <div className="space-y-2">
         <Label htmlFor="transcription_prompt">Prompt de transcrição</Label>
-        <Textarea ref={promptInputRef} id="transcription_prompt" className="h-20 resize-none leading-relaxed" placeholder="Inclua palavras-chave mencionadazx no video separadas por virgula (,)" />
+        <Textarea disabled={status !== 'waiting'} ref={promptInputRef} id="transcription_prompt" className="h-20 resize-none leading-relaxed" placeholder="Inclua palavras-chave mencionadazx no video separadas por virgula (,)" />
       </div>
 
-      <Button type="submit" className="w-full">
-        Carregar video
-        <Upload className="w-4 h-4 ml-2" />
+      <Button
+        data-success={status === 'success'}
+        disabled={status !== 'waiting'}
+        type="submit"
+        className="w-full data-[success=true]:bg-emerald-400">
+        {status === 'waiting' ? (
+          <>
+            Carregar video
+            <Upload className="w-4 h-4 ml-2" />
+          </>
+        ) : statusMessages[status]}
       </Button>
     </form>
   )
